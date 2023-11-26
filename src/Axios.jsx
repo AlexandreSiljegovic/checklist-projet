79e1394a5a20afc49822b34bf4f969cbb71cc3c4
Alexandre Siljegovic <130472981+AlexandreSiljegovic@users.noreply.github.com>
2023-11-26 23:41:39 +0100

    tailwind + axios

---
+++ src/Axios.jsx
@@ -0,0 +1,64 @@
+
+import axios from 'axios';
+
+const testUrl = 'https://greenvelvet.alwaysdata.net/pfc/ping'
+const postUrl = 'https://greenvelvet.alwaysdata.net/pfc/checklist/add'
+const apiUrl = 'https://greenvelvet.alwaysdata.net/pfc/';
+const token = 'b679e83afaf347c039f6439266e514d3cc8deb28'
+
+export const fetchDataFromApi = async () => {
+    try {
+      // Utiliser Axios pour effectuer la requête GET avec le token d'authentification
+      const responseGet = await axios.get(apiUrl, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        
+        },
+      });
+      
+    
+  
+      console.log(responseGet.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  export const postDataToApi = async () => {
+     try {
+      // Utiliser Axios pour effectuer la requête POST avec le token d'authentification
+      const responsePost = await axios.post(postUrl, {
+        headers: {
+          'Token': `${token}`,
+        
+        },
+       
+      });
+      console.log(responsePost.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+     export const test = async () => {
+      try {
+        // Utiliser Axios pour effectuer la requête POST avec le token d'authentification
+        const responsePostTest = await axios.get(testUrl, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          
+          },
+          
+          
+        });
+        console.log(responsePostTest.data);
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+
+
+
+export default axios;
