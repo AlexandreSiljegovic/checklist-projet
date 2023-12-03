44e3230c94f59c4132742ac9a4a83ed2a51a3d0c
Alexandre Siljegovic <130472981+AlexandreSiljegovic@users.noreply.github.com>
2023-12-03 05:04:44 +0100

    css à corriger c bzr cette histoire là

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,16 @@
 import {Link} from 'react-router-dom';
 
-
+import { useState, useEffect } from "react";
+import { fetchDataFromApi, deleteDataFromApi, updateDataFromApi, statutDataFromApi } from "./Axios";
+import ModifyListForm from "./ModifyListForm";
+import { format } from 'date-fns';
+import ViewList from './ViewLists';
 
 const Dashboard = () => {
-    return ( 
+return (
+ 
         <div className='dashboard-container'>
-           
+           <ViewList /> 
         <button>
             <Link className='dashboard-link' to='/form'> Se créer une liste </Link>
         </button>
--- src/ViewLists.jsx
+++ src/ViewLists.jsx
@@ -120,6 +120,7 @@ const ViewList = () => {
           )}
         </div>
       ))}
+      
     </div>
   );
 };
